6e627d149db26bdb728653c9e99316d1d9a0f21e
RamaNugraha07 <ramanugraha712@gmail.com>
2022-12-15 17:16:23 +0700

    Pada home mengganti tampilan bawah, Mengarahkan sosmed ke akun PLN, pada Getstarted mengganti nama pada class, pada view menambahkan php ke dalam perubahan daya untuk crud, di router menambahkan router perubahan daya dan migrasi, di controller menghapus bagian about

---
+++ app/Views/perubahandaya.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<header class="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="h1"><center> Layanan Pasang Baru Listrik </center></h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="h2">1. Pilih Lokasi Pemasangan</h2>
+                <p>Tentukan lokasi pasang baru listrik</p>
+                <h2 class="h2">2. Isi Detail Layanan</h2>
+                <p>Tentukan daya yang dibutuhkan, jenis layanan, dan keperluan</p>
+                <h2 class="h2">3. Isi Data SLO</h2>
+                <p>Lengkapi data SLO</p>
+                <h2 class="h2">4. Pilih Token (khusus prabayar)</h2>
+                <p>Pilih token perdana</p>
+                <h2 class="h2">5. Isi data pelanggan</h2>
+                <p>Lengkapi data diri pemohon layanan</p>
+                <div>
+                 <button class = "btn-btn-primary">   
+                <a href="getstarted"<input type="button" class="btn btn-opacity-light mr-1">Input Data Pasang Baru</a></button>
+            </div>
+        </div>
+    </div>
+
+    <footer class="jumbotron jumbotron-fluid mt-5 mb-0">
+		<div class="container text-center">Copyright &copy <?= Date('Y') ?> STIKI Malang</div>
+	</footer>
+
+    <!-- Jquery dan Bootsrap JS -->
+    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+</body>
+
+</html>
