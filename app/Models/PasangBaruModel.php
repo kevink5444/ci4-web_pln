cffeac256b5328d1e73df2f165f126b056701062
kevink5444 <kevinkurniawan901@gmail.com>
2023-01-11 10:39:33 +0700

    pasangbaru

--- app/Models/PasangBaruModel.php
+++ app/Models/PasangBaruModel.php
@@ -2,10 +2,10 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class PelangganModel extends Model
+class PasangBaruModel extends Model
 {
-    protected $table      = 'Pelanggan';
-    protected $primaryKey = 'id_pelanggan';
+    protected $table      = 'pasangbaru';
+    protected $primaryKey = 'id_pasangbaru';
     protected $returnType     = 'object';
     protected $allowedFields = ['nama', 'alamat', 'no_telp', 'daya', 'golongan', 'token_awal','idpel'];
     protected $useTimestamps = true;
